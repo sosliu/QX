e07d50488ad5f2886ea26eb50e8ac84c7cd139b8
sosliu <64636529+sosliu@users.noreply.github.com>
2023-10-12 19:39:33 +0800

    Update imaotai.js

--- boxjs/imaotai.js
+++ boxjs/imaotai.js
@@ -12,11 +12,11 @@
 hostname = app.moutai519.com.cn
 
 [rewrite_local]
-https://app.moutai519.com.cn/xhr/front/user/info url script-response-body https://github.com/sosliu/QX/blob/main/boxjs/imaotai.js
+https://app.moutai519.com.cn/xhr/front/user/info url script-response-body https://raw.githubusercontent.com/sosliu/QX/main/boxjs/imaotai.js
 
 [task_local]
 # 茅台自动预约
-0,30 9-20 * * * https://raw.githubusercontent.com/Yuheng0101/X/main/Tasks/imaotai/imaotai.js, tag=i茅台自动预约, img-url=https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/ae/f4/18/aef41811-955e-e6b0-5d23-6763c2eef1ab/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/144x144.png, enabled=true
+0,30 9-20 * * * https://raw.githubusercontent.com/sosliu/QX/main/boxjs/imaotai.js, tag=i茅台自动预约, img-url=https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/ae/f4/18/aef41811-955e-e6b0-5d23-6763c2eef1ab/AppIcon-0-0-1x_U007emarketing-0-0-0-7-0-0-sRGB-0-0-0-GLES2_U002c0-512MB-85-220-0-0.png/144x144.png, enabled=true
 ```
 ******************************************/
 const $ = new Env('i茅台'), service = $.http
